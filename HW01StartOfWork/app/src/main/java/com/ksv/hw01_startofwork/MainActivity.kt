eef3ad94b50fe59147887f4c70091707b58fd6c4
KotlyarovSergey <zews-sv@rambler.ru>
2024-03-16 16:25:11 +0300

    hw1-maket

--- HW01StartOfWork/app/src/main/java/com/ksv/hw01_startofwork/MainActivity.kt
+++ HW01StartOfWork/app/src/main/java/com/ksv/hw01_startofwork/MainActivity.kt
@@ -2,10 +2,12 @@ package com.ksv.hw01_startofwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ksv.hw01_startofwork.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
