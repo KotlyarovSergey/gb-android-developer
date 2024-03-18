10ca69a6a54ec475a168bfa9c59782003e632adc
KotlyarovSergey <zews-sv@rambler.ru>
2024-03-18 18:00:35 +0300

    Lesson-2

---
+++ Lesson2/app/src/main/java/com/ksv/gb/lesson2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ksv.gb.lesson2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, MainActivity4::class.java)
+        startActivity(intent)
+
+    }
+}
