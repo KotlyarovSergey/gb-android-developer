f7b8940719c3df528062f72d8ed8713e4cd2f5f4
KotlyarovSergey <zews-sv@rambler.ru>
2024-03-28 00:10:20 +0300

    hw-4 logic

--- HW04BasicComponents/app/src/main/java/com/ksv/hw04basiccomponents/MainActivity.kt
+++ HW04BasicComponents/app/src/main/java/com/ksv/hw04basiccomponents/MainActivity.kt
@@ -2,10 +2,98 @@ package com.ksv.hw04basiccomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import com.ksv.hw04basiccomponents.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding) {
+            textInputName.doOnTextChanged { _, _, _, _ ->
+                checkAbilityToSaveAndCalcProgress()
+            }
+
+            textInputPhone.doOnTextChanged { _, _, _, _ ->
+                checkAbilityToSaveAndCalcProgress()
+            }
+
+            radioGroupSex.setOnCheckedChangeListener { _, _ ->
+                checkAbilityToSaveAndCalcProgress()
+            }
+
+            notificationSwitch.setOnCheckedChangeListener { _, isChecked ->
+                checkAbilityToSaveAndCalcProgress()
+                checkAuthorisation.isEnabled = isChecked
+                checkNewProducts.isEnabled = isChecked
+            }
+
+            checkAuthorisation.setOnClickListener {
+                checkAbilityToSaveAndCalcProgress()
+            }
+
+            checkNewProducts.setOnClickListener {
+                checkAbilityToSaveAndCalcProgress()
+            }
+
+            btnSave.setOnClickListener {
+                save()
+            }
+        }
+    }
+
+
+    private fun checkAbilityToSaveAndCalcProgress() {
+//        abilityToSave()
+        calculateProgress()
+        binding.btnSave.isEnabled = binding.linearProgress.progress == 100
+    }
+
+//    private fun abilityToSave() {
+//        binding.btnSave.isEnabled = checkName() &&
+//                checkPhone() &&
+//                checkSex() &&
+//                checkNotify()
+//    }
+
+    private fun calculateProgress() {
+        with(binding) {
+            var progress = 0
+            if (checkName()) progress += 25
+            if (checkPhone()) progress += 25
+            if (checkSex()) progress += 25
+            if (checkNotify()) progress += 25
+
+            linearProgress.progress = progress
+
+            val text = "$progress/100"
+            tvScoresPoints.text = text
+        }
+
+
+    }
+
+    private fun checkName(): Boolean = !binding.textInputName.text.isNullOrBlank() &&
+            binding.textInputName.text.toString().length < 40
+
+    private fun checkPhone(): Boolean = !binding.textInputPhone.text.isNullOrBlank()
+
+    private fun checkSex(): Boolean = binding.radioMan.isChecked || binding.radioWoman.isChecked
+
+    private fun checkNotify(): Boolean = binding.notificationSwitch.isChecked &&
+            (binding.checkAuthorisation.isChecked ||
+                    binding.checkNewProducts.isChecked)
+
+    private fun save() {
+        Toast.makeText(
+            this,
+            getString(R.string.save_successful),
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
