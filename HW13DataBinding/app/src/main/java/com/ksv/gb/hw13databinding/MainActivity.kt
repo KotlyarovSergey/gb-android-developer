c9f274883b0ec47ee244c2661fc02c25cbf4136c
KotlyarovSergey <zews-sv@rambler.ru>
2024-05-05 00:04:19 +0300

    hw-13 cleaning

--- HW13DataBinding/app/src/main/java/com/ksv/gb/hw13databinding/MainActivity.kt
+++ HW13DataBinding/app/src/main/java/com/ksv/gb/hw13databinding/MainActivity.kt
@@ -2,11 +2,7 @@ package com.ksv.gb.hw13databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.View
 import androidx.activity.viewModels
-import androidx.core.widget.addTextChangedListener
-import androidx.lifecycle.lifecycleScope
 import com.ksv.gb.hw13databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,119 +15,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-
-
-        //      TODO        УДАЛИТЬ
-        binding.stateChange.setOnClickListener {
-            viewModel.searchJobCancel(this)
-//            viewModel.stateChange()
-//            binding.stateTextView.text = viewModel.state.value.toString()
-//            binding.searchingText = binding.searchEdit.text.toString()
-////            binding.textView.text = viewModel.resultText
-//            binding.invalidateAll()
-        }
-
-
-
-        restoreSavedState(savedInstanceState)
-//        setViewsListeners()
-//        setViewModelListeners()
     }
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putBoolean(SEARCH_BUTTON_ENABLED, binding.searchButton.isEnabled)
-        outState.putBoolean(SEARCH_EDIT_ENABLED, binding.searchEdit.isEnabled)
-        outState.putString(SEARCH_RESULT, binding.textView.text.toString())
-        super.onSaveInstanceState(outState)
-    }
-
-    private fun restoreSavedState(savedInstanceState: Bundle?){
-//        binding.searchButton.isEnabled =
-//            savedInstanceState?.getBoolean(SEARCH_BUTTON_ENABLED, false) ?: false
-        binding.searchEdit.isEnabled =
-            savedInstanceState?.getBoolean(SEARCH_EDIT_ENABLED, true) ?: true
-        val defaultResult = getString(R.string.tv_default)
-        binding.textView.text =
-            savedInstanceState?.getString(SEARCH_RESULT, defaultResult) ?: defaultResult
-    }
-
-    private fun setViewsListeners(){
-//        binding.searchEdit.addTextChangedListener {
-//            viewModel.searchEditChange()
-////            viewModel.searchEditChange(it.toString())
-//        }
-
-//        binding.searchButton.setOnClickListener {
-//            val searchedText = binding.searchEdit.text.toString()
-//            viewModel.onSearchClick(searchedText)
-//        }
-
-    }
-
-    private fun setViewModelListeners(){
-//        lifecycleScope.launchWhenStarted {
-//            viewModel.state
-//                .collect { state ->
-//                    setViewsWithSearchState(state)
-//                }
-//        }
-
-
-//        lifecycleScope.launchWhenStarted {
-//            viewModel.result
-//                .collect {
-//                    showResult(it)
-//                }
-//        }
-    }
-
-    private fun showResult(result: String?){
-        binding.textView.text = when (result) {
-            null -> getString(R.string.tv_default)
-            else -> {
-                StringBuilder()
-                    .append(getString(R.string.tv_result_prefix))
-                    .append(result)
-                    .append(getString(R.string.tv_result_postfix))
-                    .toString()
-            }
-        }
-    }
-
-    private fun setViewsWithSearchState(state: State) {
-        when (state) {
-            State.Search -> {
-                with(binding) {
-                    progress.visibility = View.VISIBLE
-                    searchEdit.isEnabled = false
-                    searchButton.isEnabled = false
-                }
-            }
-            is State.Normal -> {
-                with(binding) {
-                    searchEdit.isEnabled = true
-                    searchButton.isEnabled = true
-                    progress.visibility = View.GONE
-                }
-            }
-            is State.Insufficient -> {
-                with(binding) {
-                    searchEdit.isEnabled = true
-                    searchButton.isEnabled = false
-                    progress.visibility = View.GONE
-                }
-            }
-        }
-    }
-
-    companion object {
-        private const val SEARCH_BUTTON_ENABLED = "search_button_enable"
-        private const val SEARCH_EDIT_ENABLED = "search_edit_enable"
-        private const val SEARCH_RESULT= "search_result"
-    }
-
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
 }
--- HW13DataBinding/app/src/main/java/com/ksv/gb/hw13databinding/State.kt
+++ HW13DataBinding/app/src/main/java/com/ksv/gb/hw13databinding/State.kt
@@ -3,15 +3,7 @@ package com.ksv.gb.hw13databinding
 sealed class State(
     open val resultText: String? = null
 ){
-    data class Normal(override val resultText: String?) : State(resultText = resultText){
-        override fun toString(): String {
-            return "Normal"
-        }
-    }
+    data class Normal(override val resultText: String?) : State(resultText = resultText)
     data object Search : State()
-    data class Insufficient(override val resultText: String?) : State(resultText = resultText){
-        override fun toString(): String {
-            return "Insufficient"
-        }
-    }
+    data class Insufficient(override val resultText: String?) : State(resultText = resultText)
 }
